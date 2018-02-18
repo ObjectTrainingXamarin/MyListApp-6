e00175697844fcf031a87bf54ef59b1d9a9d97c8
Tim Hastings <thastings@objectconsulting.com.au>
2018-02-18 12:19:03 +1100

    Add project files.

---
+++ MyListApp-5/MyListApp_5/Database/Database.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c), 2017. Object Training Pty Ltd. All rights reserved.
+// Written by Tim Hastings.
+//
+
+using System;
+using SQLite;
+namespace MyListApp
+{
+	public static class Database
+	{
+		public static SQLiteConnection connection { get; set; }
+		public const String dbName = "MyListApp.db";
+
+		public static void CreateDatabase(String dbname)
+		{
+			connection = DatabaseServices.OpenDatabase(dbName);
+			DatabaseServices.CreateTable<ItemDB>(connection);
+            DatabaseServices.CreateTable<UserDB>(connection);
+        }
+
+		public static void LoadModel()
+		{
+			ItemDB.LoadAll();
+            UserDB.LoadAll();
+		}
+
+		public static void SaveModel()
+		{
+			ItemDB.SaveAll();
+            UserDB.SaveAll();
+
+			//	Reload so we have item ids which are allocate by the DB.
+			ItemDB.LoadAll();	
+		}
+
+	}
+}
